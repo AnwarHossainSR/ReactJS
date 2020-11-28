f1f483de62f8840533db70d2ca8c61df77765d3b
AnwarHossainSR <43861146+AnwarHossainSR@users.noreply.github.com>
2020-11-28 13:25:46 +0600

    Add files via upload

---
+++ ReactJS/Component/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import Heading from './Heading';
+import List from './List';
+import Param from './Para';
+function App(){
+    return(
+        <>
+            <Heading />
+            <Param />
+            <List />
+        </>
+    );
+}
+
+export default App;
---
+++ ReactJS/Component/Heading.jsx
@@ -0,0 +1,5 @@
+import react from 'react';
+function Heading(){
+    return <h1>This is Mahedi</h1>;
+}
+export default Heading;
